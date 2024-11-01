cc1005e26fc19e943a9a91274fba3623df445a4b
federicodiazbobillo <federicodiazbobillo@gmail.com>
2024-11-01 12:31:07 -0300

    Update inicio.py

--- app/models/inicio.py
+++ app/models/inicio.py
@@ -2,7 +2,8 @@ from app import db
 
 class Inicio(db.Model):
     __tablename__ = 'inicio'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(200), nullable=True)
 
     def __repr__(self):
-        return f"<Inicio(title='{self.title}')>"
+        return f'<Inicio {self.title}>'
